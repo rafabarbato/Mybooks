81dd573503cfbe511ae7c3f911c4f94ecc417ab5
Rafael Barbato <kadeshbrt@gmail.com>
2023-07-03 19:58:00 -0300

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,35 @@
-/* import { useState } from 'react'
+import { useState } from 'react'
 import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg' */
-import "./index.css";
-import { Link } from 'react-router-dom'
+import viteLogo from '/vite.svg'
+import './App.css'
+
 function App() {
- /*  */
+  const [count, setCount] = useState(0)
 
   return (
-    <h1 className="text-3xl font-bold underline">
-    Hello world!
-    </h1>
+    <>
+      <div>
+        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank" rel="noreferrer">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button className="bg-red-600" onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
   )
-
 }
 
 export default App
