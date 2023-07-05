8d522ea915c2624daf7e0ca2f3e76fdbc165e9e1
Rafael Barbato <kadeshbrt@gmail.com>
2023-07-05 20:35:59 -0300

    feat: adicionando footer

--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,12 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { Navbar } from './components/Navbar/index.jsx'
+import { Footer } from './Components/Footer/index.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Navbar />
     <App />
+    <Footer />
   </React.StrictMode>,
 )
