e1dc23ea46148c0f97766a923798cb9073464ef0
Rafael Barbato <kadeshbrt@gmail.com>
2023-07-04 23:50:02 -0300

    feat():Integrando com API do Google Books

--- src/Components/Sections/SectionHomeContent.jsx
+++ src/Components/Sections/SectionHomeContent.jsx
@@ -1,68 +1,174 @@
-import { HeaderTitle } from '../HeaderTitle'
-import { Swiper, SwiperSlide } from 'swiper/react';
-import { RatingsStar } from '../partials/RatingsStar'
-import 'swiper/css';
+import { HeaderTitle } from "../HeaderTitle";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { RatingsStar } from "../partials/RatingsStar";
+import { useEffect, useState } from "react";
+import "swiper/css";
 export const SectionHomeContent = () => {
+  const [books, setBooks] = useState([]);
 
-    //crie uma função que limita os caracteres do titulo 
-    
-    function limitarCaracteres(titulo, limite) {
-        if (titulo.length > limite) {
-          titulo = titulo.substring(0, limite) + "...";
-        }
-        return titulo;
-      }
-    return (
-        <div className='mt-14 p-5 overflow-y-auto w-full ml-64'>
-            <HeaderTitle text='Descubra' />
-            <div className='flex  mt-4'>
-                <Swiper slidesPerView={6} spaceBetween={12}>
-                    <SwiperSlide> <img src="/img/categories/livros_infantis.png" alt="livros para crianças" className='aspect-square w-full h-full' /></SwiperSlide>
-                    <SwiperSlide> <img src="/img/categories/livros_na_categoria_medieval.png" alt="livros para crianças" className='aspect-square w-full h-full' /></SwiperSlide>
-                    <SwiperSlide> <img src="/img/categories/livros_para_relaxar.png" alt="livros para crianças" className='aspect-square w-full h-full' /></SwiperSlide>
-                    <SwiperSlide> <img src="/img/categories/livros_relax.png" alt="livros para crianças" className='aspect-square w-full h-full' /></SwiperSlide>
-                    <SwiperSlide> <img src="/img/categories/livros.png" alt="livros para crianças" className='aspect-square w-full h-full' /></SwiperSlide>
-                    <SwiperSlide> <img src="/img/categories/livros_infantis.png" alt="livros para crianças" className='aspect-square w-full h-full' /></SwiperSlide>
-                    <SwiperSlide> <img src="/img/categories/livros_infantis.png" alt="livros para crianças" className='aspect-square w-full h-full' /></SwiperSlide>
-                    <SwiperSlide> <img src="/img/categories/livros_na_categoria_medieval.png" alt="livros para crianças" className='aspect-square w-full h-full' /></SwiperSlide>
-                    <SwiperSlide> <img src="/img/categories/livros_para_relaxar.png" alt="livros para crianças" className='aspect-square w-full h-full' /></SwiperSlide>
-                    <SwiperSlide> <img src="/img/categories/livros_relax.png" alt="livros para crianças" className='aspect-square w-full h-full' /></SwiperSlide>
-                    <SwiperSlide> <img src="/img/categories/livros.png" alt="livros para crianças" className='aspect-square w-full h-full' /></SwiperSlide>
-                    <SwiperSlide> <img src="/img/categories/livros_infantis.png" alt="livros para crianças" className='aspect-square w-full h-full' /></SwiperSlide>
-                </Swiper>
+  function limitarCaracteres(titulo, limite) {
+    if (titulo.length > limite) {
+      titulo = titulo.substring(0, limite) + "...";
+    }
+    return titulo;
+  }
 
+  async function getBooks() {
+    try {
+      const response = await fetch(
+        "https://www.googleapis.com/books/v1/volumes?q=search+terms"
+      );
 
+      const data = await response.json();
+      setBooks(data);
+      console.log(data);
+    } catch (erro) {
+      console.log(erro);
+    }
+  }
 
+  useEffect(() => {
+    getBooks();
+  }, []);
+
+  return (
+    <div className="mt-14 p-5 overflow-y-auto w-full ml-64">
+      <HeaderTitle text="Descubra" />
+      <div className="flex  mt-4">
+        <Swiper slidesPerView={6} spaceBetween={12}>
+          <SwiperSlide>
+            {" "}
+            <img
+              src="/img/categories/livros_infantis.png"
+              alt="livros para crianças"
+              className="aspect-square w-full h-full"
+            />
+          </SwiperSlide>
+          <SwiperSlide>
+            {" "}
+            <img
+              src="/img/categories/livros_na_categoria_medieval.png"
+              alt="livros para crianças"
+              className="aspect-square w-full h-full"
+            />
+          </SwiperSlide>
+          <SwiperSlide>
+            {" "}
+            <img
+              src="/img/categories/livros_para_relaxar.png"
+              alt="livros para crianças"
+              className="aspect-square w-full h-full"
+            />
+          </SwiperSlide>
+          <SwiperSlide>
+            {" "}
+            <img
+              src="/img/categories/livros_relax.png"
+              alt="livros para crianças"
+              className="aspect-square w-full h-full"
+            />
+          </SwiperSlide>
+          <SwiperSlide>
+            {" "}
+            <img
+              src="/img/categories/livros.png"
+              alt="livros para crianças"
+              className="aspect-square w-full h-full"
+            />
+          </SwiperSlide>
+          <SwiperSlide>
+            {" "}
+            <img
+              src="/img/categories/livros_infantis.png"
+              alt="livros para crianças"
+              className="aspect-square w-full h-full"
+            />
+          </SwiperSlide>
+          <SwiperSlide>
+            {" "}
+            <img
+              src="/img/categories/livros_infantis.png"
+              alt="livros para crianças"
+              className="aspect-square w-full h-full"
+            />
+          </SwiperSlide>
+          <SwiperSlide>
+            {" "}
+            <img
+              src="/img/categories/livros_na_categoria_medieval.png"
+              alt="livros para crianças"
+              className="aspect-square w-full h-full"
+            />
+          </SwiperSlide>
+          <SwiperSlide>
+            {" "}
+            <img
+              src="/img/categories/livros_para_relaxar.png"
+              alt="livros para crianças"
+              className="aspect-square w-full h-full"
+            />
+          </SwiperSlide>
+          <SwiperSlide>
+            {" "}
+            <img
+              src="/img/categories/livros_relax.png"
+              alt="livros para crianças"
+              className="aspect-square w-full h-full"
+            />
+          </SwiperSlide>
+          <SwiperSlide>
+            {" "}
+            <img
+              src="/img/categories/livros.png"
+              alt="livros para crianças"
+              className="aspect-square w-full h-full"
+            />
+          </SwiperSlide>
+          <SwiperSlide>
+            {" "}
+            <img
+              src="/img/categories/livros_infantis.png"
+              alt="livros para crianças"
+              className="aspect-square w-full h-full"
+            />
+          </SwiperSlide>
+        </Swiper>
+      </div>
+      <div className="divide-x-2 divider"></div>
+      <HeaderTitle text="Lançamentos " />
+      <div className="flex mt-4">
+        <Swiper slidesPerView={8} spaceBetween={12}>
+          {books &&  books?.items?.map(book => {
+            return <SwiperSlide key={book.id} className="flex flex-col">
+          
+            <div className="aspect-[55/90] object-contain">
+              <label htmlFor="my_modal_7" className="cursor-pointer">
+                <img
+                  src={book.volumeInfo.imageLinks ? book.volumeInfo.imageLinks.thumbnail : '/img/books/no_cover_thumb.png'}
+                  alt={`${book.volumeInfo.title}`}
+                  className=" w-full h-full"
+                />
+              </label>
             </div>
-            <div className='divide-x-2 divider'></div>
-            <HeaderTitle text='Lançamentos ' />
-            <div className='flex mt-4'>
-
-                <Swiper slidesPerView={8} spaceBetween={12}>
-                    <SwiperSlide className='flex flex-col'> <div className='aspect-[55/90] object-contain'><label htmlFor="my_modal_7" className="btn"><img src="/img/books/a_vida_mentirosa_dos_adultos.png" alt="livros para crianças" className=' w-full h-full' /></label></div><div className='flex flex-col'>
-                        <strong className='text-gray-900 mt-1 text-base' title="A vida Mentirosa dos Adultos">{limitarCaracteres('A Vida Mentirosa dos Adultos', 28)}</strong><p>William Martins</p><p><RatingsStar count={1}/> </p></div> </SwiperSlide>
-                    <SwiperSlide className='flex flex-col'> <div className='aspect-[55/90] object-contain'><img src="/img/books/ultimo_turno.png" alt="livros para crianças" className=' w-full h-full' /></div><div className='flex flex-col'>
-                        <strong className='text-gray-900 mt-1 text-base'>{limitarCaracteres('Último Turno', 14)}</strong><p>William Martins</p><p><RatingsStar count={2}/></p></div> </SwiperSlide>
-                    <SwiperSlide className='flex flex-col'> <div className='aspect-[55/90] object-contain'><img src="/img/books/o_poder_do_habito.png" alt="livros para crianças" className=' w-full h-full' /></div><div className='flex flex-col'>
-                        <strong className='text-gray-900 mt-1 text-base'>O Poder do Hábito</strong><p>Arthur Rocha</p><p><RatingsStar count={3}/> </p></div> </SwiperSlide>
-                    <SwiperSlide className='flex flex-col'> <div className='aspect-[55/90] object-contain'><img src="/img/books/rainhas_da_noite.png" alt="livros para crianças" className=' w-full h-full' /></div><div className='flex flex-col'>
-                        <strong className='text-gray-900 mt-1 text-base'>A Rainha da noite</strong><p>Arthur Rocha</p><p><RatingsStar count={4}/> </p></div> </SwiperSlide>
-                    <SwiperSlide className='flex flex-col'> <div className='aspect-[55/90] object-contain'><img src="/img/books/os_oito_vestidos_dior.png" alt="livros para crianças" className=' w-full h-full' /></div><div className='flex flex-col'>
-                        <strong className='text-gray-900 mt-1 text-base'>A vida mentirosa dos adultos</strong><p>Rafael Barbato</p><p><RatingsStar count={3}/></p></div> </SwiperSlide>
-                    <SwiperSlide className='flex flex-col'> <div className='aspect-[55/90] object-contain'><img src="/img/books/nunca_vi_a_chuva.png" alt="livros para crianças" className=' w-full h-full' /></div><div className='flex flex-col'>
-                        <strong className='text-gray-900 mt-1 text-base'>A vida mentirosa dos adultos</strong><p>Rafael Barbato</p><p><RatingsStar count={4}/></p></div> </SwiperSlide>
-                    <SwiperSlide className='flex flex-col'> <div className='aspect-[55/90] object-contain'><img src="/img/books/a_vida_mentirosa_dos_adultos.png" alt="livros para crianças" className=' w-full h-full' /></div><div className='flex flex-col'>
-                        <strong className='text-gray-900 mt-1 text-base'>A vida mentirosa dos adultos</strong><p>Arthur Rocha</p><p><RatingsStar count={5}/></p></div> </SwiperSlide>
-                    <SwiperSlide className='flex flex-col'> <div className='aspect-[55/90] object-contain'><img src="/img/books/a_vida_mentirosa_dos_adultos.png" alt="livros para crianças" className=' w-full h-full' /></div><div className='flex flex-col'>
-                        <strong className='text-gray-900 mt-1 text-base'>A vida mentirosa dos adultos</strong><p>William Martins</p><p><RatingsStar count={1}/></p></div> </SwiperSlide>
-                    <SwiperSlide className='flex flex-col'> <div className='aspect-[55/90] object-contain'><img src="/img/books/a_vida_mentirosa_dos_adultos.png" alt="livros para crianças" className=' w-full h-full' /></div><div className='flex flex-col'>
-                        <strong className='text-gray-900 mt-1 text-base'>A vida mentirosa dos adultos</strong><p>Rafael Barbato</p><p><RatingsStar count={2}/></p></div> </SwiperSlide>
-
-
-                </Swiper>
-            </div>
-            <div className='divide-x-2 divider'></div>
-            <HeaderTitle text='Lançamentos ' />
+            <div className="flex flex-col">
+              <strong
+                className="text-gray-900 mt-1 text-base"
+                title={book.volumeInfo.title}
+              >
+                {limitarCaracteres(book.volumeInfo.title, 20)}
+              </strong>
+              <p>{book.volumeInfo.authors ? book.volumeInfo.authors : "Desconhecido"}</p>
+              <p>
+                <RatingsStar count={5} />{" "}
+              </p>
+       
+     </div>{" "}
+          </SwiperSlide>
+          })}
+        </Swiper>
+      </div>
+      <div className='divide-x-2 divider'></div>
+            <HeaderTitle text='Recentemente Adicionados' />
             <div className='flex mt-4'>
 
                 <Swiper slidesPerView={8} spaceBetween={12}>
@@ -88,14 +194,14 @@ export const SectionHomeContent = () => {
 
                 </Swiper>
             </div>
-            <input type="checkbox" id="my_modal_7" className="modal-toggle" />
-<div className="modal">
-  <div className="modal-box">
-    <h3 className="text-lg font-bold">Hello!</h3>
-    <p className="py-4">HIDDEN MODAL</p>
-  </div>
-  <label className="modal-backdrop" htmlFor="my_modal_7">Close</label>
-</div>
+      <input type="checkbox" id="my_modal_7" className="modal-toggle" />
+      <div className="modal">
+        <div className="modal-box">
+          <h3 className="text-lg font-bold">Hello!</h3>
+          <p className="py-4">HIDDEN MODAL</p>
         </div>
-    )
-}
+        <label className="modal-backdrop" htmlFor="my_modal_7"></label>
+      </div>
+    </div>
+  );
+};
