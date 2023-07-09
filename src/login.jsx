64bd3e04b42ba2ab00778263d218e6283d88fffa
ArthurMaciel95 <arthurnmrocha@gmail.com>
2023-07-09 19:37:59 -0300

    merge

--- src/login.jsx
+++ src/login.jsx
@@ -14,6 +14,7 @@ const Login = () => {
          
           onSuccess={(credentialResponse) => {
             navigate('/')
+            console.log(credentialResponse)
             setGoogleCrediential(credentialResponse.credential)
            
           }}
