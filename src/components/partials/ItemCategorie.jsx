eb12028ab16f526ca815034964c1f5f5b01ffd1d
Rafael Barbato <kadeshbrt@gmail.com>
2023-07-09 13:54:18 -0300

    style(ui): trocando cor de texto e icone

--- src/components/partials/ItemCategorie.jsx
+++ src/components/partials/ItemCategorie.jsx
@@ -2,11 +2,11 @@ import { Icon } from '@iconify/react'
 
 
 // eslint-disable-next-line react/prop-types
-export const ItemCategorie = ({ text }) => {
+export const ItemCategorie = ({ text, icon}) => {
     return (
         <li className='flex items-center hover:bg-gray-100 transition-colors p-2 cursor-pointer rounded-sm'>
-            <Icon icon="carbon:categories" color="#8471b0" />
-            <p className='ml-2 text-gray-700'>{text}</p>
+            {icon}
+            <p className='ml-2 text-gray-400'>{text}</p>
         </li>
     )
 }
